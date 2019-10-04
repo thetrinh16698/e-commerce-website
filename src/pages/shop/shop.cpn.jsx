74915efa5499db29a7a5ee33e139b76d3c591884
thetrinh166 <thetrinh166@gmail.com>
2019-10-04 11:38:32 +0300

    update stripe api for payment and the title of the website

--- src/pages/shop/shop.cpn.jsx
+++ src/pages/shop/shop.cpn.jsx
@@ -4,6 +4,8 @@ import { Route } from 'react-router-dom';
 import CollectionsOverview from '../../components/collections-overview/collections-overview.cpn';
 import CollectionPage from '../collection/collection.cpn';
 
+import './shop.style.scss'
+
 const ShopPage = ({ match }) => (
     <div className='shop-page'>
         <Route exact path={`${match.path}`} component={CollectionsOverview} />
