37278a2f42ea3217af92682ff9f1de88adf878d6
thetrinh16698 <thetrinh166@gmail.com>
2020-12-07 16:42:00 +0200

    adding gzipping feature to heroku deployment to decrease the loading size of the file on deployment

--- client/src/components/error-boundary/error-boundary.cpn.jsx
+++ client/src/components/error-boundary/error-boundary.cpn.jsx
@@ -1,35 +1,39 @@
-import React from 'react';
+import React from "react";
 
-import { ErrorImageOverlay, ErrorImageContainer, ErrorImageText} from './error-boundary.style';
+import {
+  ErrorImageOverlay,
+  ErrorImageContainer,
+  ErrorImageText,
+} from "./error-boundary.style";
 
 class ErrorBoundary extends React.Component {
-    constructor() {
-        super();
+  constructor() {
+    super();
 
-        this.state = {
-            hasErrored: false
-        };
-    }
+    this.state = {
+      hasErrored: false,
+    };
+  }
 
-    static getDerivedStateFromError(error) {
-        return { hasErrored: true };
-    }
+  static getDerivedStateFromError(error) {
+    return { hasErrored: true };
+  }
 
-    componentDidCatch(error, info) {
-        console.log(error);
-    }
+  componentDidCatch(error, info) {
+    console.log(error);
+  }
 
-    render() {
-        if (this.state.hasErrored) {
-            return (
-              <ErrorImageOverlay>
-                <ErrorImageContainer imageUrl='https://i.imgur.com/A040Lxr.png' />
-                <ErrorImageText>Sorry this page is not available now!</ErrorImageText>
-              </ErrorImageOverlay>
-            );
-        }
-        return this.props.children;
+  render() {
+    if (this.state.hasErrored) {
+      return (
+        <ErrorImageOverlay>
+          <ErrorImageContainer imageUrl="https://i.imgur.com/A040Lxr.png" />
+          <ErrorImageText>Sorry this page is not available now!</ErrorImageText>
+        </ErrorImageOverlay>
+      );
     }
+    return this.props.children;
+  }
 }
 
 export default ErrorBoundary;
